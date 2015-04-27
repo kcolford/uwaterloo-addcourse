5ef863c2b86da67991150ba472951de25a637c3d
Kieran Colford <colfordk@gmail.com>
2015-04-26 23:30:48 -0400

    added windows upload batchfile

--- bump.sh
+++ bump.sh
@@ -3,4 +3,8 @@
 sed -i "/__version__/s/'.*'/'$1'/" addcourse/version.py
 git commit -a -m "version bump to v$1"
 git tag -s "v$1" -m "version $1"
-
+rm -rf dist
+python setup.py sdist bdist_egg
+python3 setup.py bdist_egg
+python setup.py register
+twine upload dist/*
---
+++ upload.bat
@@ -0,0 +1,2 @@
+python setup.py bdist_msi_fixed bdist_wininst upload
+python3 setup.py bdist_msi_fixed bdist_wininst upload
